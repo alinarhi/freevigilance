e6a25014bf881b29de989c3859c4b3e8d514c374
Alina <alinarhi20@gmail.com>
2025-11-06 00:02:58 +0300

    feat(frontend): add ability to post comments on task page and adjust comments ui

--- frontend/src/views/TaskView.vue
+++ frontend/src/views/TaskView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import TaskCard from '@/components/TaskCard.vue'
-import { onMounted, ref } from 'vue'
+import { nextTick, onMounted, ref, useTemplateRef, watchEffect } from 'vue'
 
 import type { Task, Comment, LogEntry } from '@/api-client'
 import { TasksApi } from '@/api-client'
@@ -17,14 +17,12 @@ const taskApi = new TasksApi(undefined, undefined, apiAxios)
 const ID = Number(router.currentRoute.value.params.id as string)
 const task = ref<Task | null>(null)
 
+const comment = ref<Comment>({ text: '' })
 const comments = ref<Comment[]>([])
 const changelog = ref<LogEntry[]>([])
 const notFound = ref(false)
 
-const actions = new Map();
-actions.set('create', 'создал(-а)')
-actions.set('update', 'изменил(-а)')
-actions.set('delete', 'удалил(-а)')
+const commentsListElement = useTemplateRef('comments-list')
 
 const fetchData = async () => {
     try {
@@ -39,7 +37,6 @@ const fetchData = async () => {
         if (isAxiosError(error)) {
             if (error.response?.status === 404) {
                 notFound.value = true
-                //   router.push({ name: 'tasks' })
             } else {
                 handleAxiosError(error)
             }
@@ -47,6 +44,30 @@ const fetchData = async () => {
     }
 }
 
+const postComment = async () => {
+    try {
+        const res = await taskApi.tasksCommentsCreate(ID, comment.value)
+        if (res.status === 201) {
+            comments.value.push(res.data)
+            comment.value.text = ''
+        }
+    } catch (error) {
+        console.error(error)
+        if (isAxiosError(error)) {
+            handleAxiosError(error)
+        }
+    }
+}
+
+// Autoscroll to the latest comment
+watchEffect(async () => {
+    // Access comments to track changes
+    comments.value.length
+    await nextTick()
+    if (commentsListElement.value) {
+        commentsListElement.value.scrollTop = commentsListElement.value.scrollHeight
+    }
+})
 
 onMounted(() => {
     fetchData()
@@ -54,39 +75,52 @@ onMounted(() => {
 </script>
 
 <template>
-    <div v-if="notFound || !task" class="text-2xl text-center text-gray-600 p-10">404 Задача не найдена.</div>
-    <div v-else class="flex h-full overflow-hidden gap-4">
+    <div v-if="notFound || !task" class="text-2xl text-center text-gray-600 p-10">
+        <p>404</p>
+        <p>Задача не найдена</p>
+    </div>
+    <div v-else class="flex h-full overflow-hidden gap-2">
         <!-- Left Column -->
-        <div class="w-3/5 flex-none flex flex-col overflow-hidden gap-4">
+        <div class="w-3/5 flex-none flex flex-col overflow-hidden gap-2">
             <!-- Task Card -->
-            <div class="h-2/3 flex-none">
+            <div class="h-3/5 grow">
                 <TaskCard class="rounded-xl shadow-md" :show-buttons="false" :task="task" />
             </div>
 
             <!-- Comments -->
-            <div class="h-1/3 flex flex-col overflow-hidden bg-white rounded-xl shadow-md p-4">
+            <div :class="{ 'h-2/5': comments.length > 0, 'h-auto': comments.length === 0 }"
+                class="flex flex-col overflow-hidden bg-white rounded-xl shadow-md p-4 space-y-2">
                 <h2 class="text-lg font-semibold">Комментарии</h2>
-                <ul class="flex-1 overflow-y-auto space-y-3">
-                    <li v-for="(comment, index) in comments" :key="index">
-                        <div v-if="comment.created_by! === userStore.user?.id" class="text-end">
-                            <div class="p-3 rounded-lg">{{ comment.text }}</div>
-                            <div class="text-sm text-gray-700">{{ comment.created_by_display }}</div>
-                            <div class="text-xs text-gray-500">{{ new
-                                Date(comment.created_at!).toLocaleString('ru-RU') }}</div>
+                <ul ref="comments-list" class="flex-1 overflow-y-auto bg-white rounded-xl p-2 space-y-3">
+                    <li v-for="comment in comments" :key="comment.id">
+                        <div v-if="comment.created_by! === userStore.user?.id" class="flex justify-end text-end">
+                            <div class="w-fit">
+                                <div class="bg-gray-100 p-3 rounded-lg">{{ comment.text }}</div>
+                                <div class="text-sm text-gray-700">{{ comment.created_by_display }}</div>
+                                <div class="text-xs text-gray-500">{{ new
+                                    Date(comment.created_at!).toLocaleString('ru-RU') }}</div>
+                            </div>
                         </div>
                         <div v-else class="w-fit">
-                            <div class="p-3rounded-lg">{{ comment.text }}</div>
+                            <div class="bg-gray-100 p-3 rounded-lg">{{ comment.text }}</div>
                             <div class="text-sm text-gray-700">{{ comment.created_by_display }}</div>
                             <div class="text-xs text-gray-500">{{ new
                                 Date(comment.created_at!).toLocaleString('ru-RU') }}</div>
                         </div>
                     </li>
                 </ul>
+                <!-- Add Comment Section -->
+                <form @submit.prevent="postComment" class="flex gap-4 items-center">
+                    <input v-model="comment.text" placeholder="Комментарий..."
+                        class="flex-1 input rounded-lg border border-gray-300 p-3 bg-gray-50 shadow-md" required />
+                    <button
+                        class="cursor-pointer px-6 py-3 rounded-xl shadow-md font-bold text-white bg-teal-600 hover:bg-teal-700">Отправить</button>
+                </form>
             </div>
         </div>
 
 
-        <!-- Right Column -->
+        <!-- Right Column: Changelog -->
         <div class="w-2/5 flex flex-col overflow-hidden bg-white rounded-xl shadow-md p-4 wrap-anywhere">
             <h2 class="text-lg font-semibold mb-2">История изменений</h2>
             <ul class="flex-1 overflow-y-auto divide-y divide-gray-300">
