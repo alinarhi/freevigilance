1fb4e81342d09bd59161baf43905bbb8e2e75cde
Alina <alinarhi20@gmail.com>
2025-11-09 16:33:06 +0300

    feat(frontend): add logout button on topbar, refine sidebar UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
-import { RouterLink, RouterView, useRoute } from 'vue-router'
+import { RouterLink, RouterView, useRoute, useRouter } from 'vue-router'
 import { useUserStore } from './stores/user';
 import { ref, computed, onMounted } from 'vue';
+import { UserIcon, ArrowRightStartOnRectangleIcon } from '@heroicons/vue/24/solid';
 
 const userStore = useUserStore()
 const route = useRoute()
-const showSidebar = computed(() => route.name !== 'login') 
+const router = useRouter()
+const showSidebar = computed(() => route.name !== 'login')
+
+const onLogout = () => {
+  userStore.logout()
+  router.push('logout')
+}
 </script>
 
 <template>
   <div class="flex flex-col h-screen overflow-hidden">
     <!-- Topbar -->
-    <header><h1 class="text-white bg-teal-700 text-2xl font-extrabold p-4">Freevigilance</h1></header>
+    <header class="flex justify-end bg-teal-700 text-white p-4">
+      <h1 class="flex-1 text-2xl font-extrabold">Freevigilance</h1>
+      <button v-if="userStore.user" @click="onLogout" class="flex-none inline-flex items-center gap-2 cursor-pointer font-semibold">
+        Выйти
+        <ArrowRightStartOnRectangleIcon class="size-5"/>
+      </button>
+    </header>
 
     <div class="flex-1 flex overflow-hidden">
-    <!-- Sidebar (Navigation)   -->
-    <aside v-if="showSidebar" class="w-64 flex-none text-white p-4 bg-teal-700/70">
-      <div v-if="userStore.user">
+    <!-- Sidebar  -->
+    <aside v-if="showSidebar" class="w-64 overflow-y-auto flex-none text-white p-4 bg-teal-700/70">
+      <!-- User Info -->
+      <div v-if="userStore.user" class="flex flex-col items-center mt-6 mb-6">
+        <UserIcon class="size-14 mb-2"/>
         <h2 class="font-bold text-lg">{{ userStore.user.first_name + ' ' + userStore.user.last_name }}</h2>
         <p class="text-gray-200 italic">{{ userStore.user.email }}</p>
         <p class="font-semibold">{{ userStore.user.username }}</p>
@@ -24,8 +39,9 @@ const showSidebar = computed(() => route.name !== 'login')
       <br>
       <hr class="text-teal-700">
       <br>
+      <!-- Navigation -->
       <nav>
-        <ul class="space-y-2 text-gray-100 font-semibold">
+        <ul class="space-y-2 text-lg text-gray-200 font-semibold">
           <li><RouterLink to="/tasks" activeClass="text-white font-bold">Задачи</RouterLink></li>
           <li><RouterLink to="/pvas" activeClass="text-white font-bold">Договоры</RouterLink></li>
           <li><RouterLink to="/auditlog" activeClass="text-white font-bold">Журнал действий</RouterLink></li>
