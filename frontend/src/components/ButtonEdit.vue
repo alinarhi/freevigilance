eecb1b402305e0251fb54afff85d9276f33c02b0
Alina <alinarhi20@gmail.com>
2025-11-12 14:10:26 +0300

    refactor(frontend): add components for buttons

---
+++ frontend/src/components/ButtonEdit.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { PencilIcon } from '@heroicons/vue/24/solid';
+</script>
+
+<template>
+    <button @click="$emit('click')" title="Редактировать"
+        class="cursor-pointer p-3 rounded-lg hover:bg-gray-100">
+        <PencilIcon class="size-5 text-teal-600 hover:text-teal-700" />
+    </button>
+</template>
---
+++ frontend/src/components/ButtonFilter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { FunnelIcon } from '@heroicons/vue/24/solid';
+</script>
+
+<template>
+    <button @click="$emit('click')" title="Фильтры" class="cursor-pointer p-3 rounded-lg hover:bg-gray-200">
+        <FunnelIcon class="size-7 text-teal-600 hover:text-teal-700" />
+    </button>
+</template>
